5127e878fcfa04b57cf074b5e337f8c5bc5f416a
Kevin <98636238+Pixel-Bath@users.noreply.github.com>
2022-02-25 14:26:01 -0500

    Add files via upload

---
+++ NumberTable.java
@@ -0,0 +1,48 @@
+// Kevin Koepp
+// CSE 174
+// 2-25-2022
+// Lab 5
+// This program makes a multiplication table based upon inputs
+// for the table's columns and rows.
+
+
+// import scanner
+import java.util.Scanner;
+
+public class NumberTable {
+  public static void main(String[] args) {
+  
+    Scanner ip = new Scanner(System.in);
+    
+    // The number MUST be less than or equal to 20 for BOTH
+    // columns and rows.
+    int column;
+    do {
+      System.out.print("How many columns should be in the table: ");
+      column = ip.nextInt();
+    } while (column > 20);
+    
+    int row;
+    do {
+      System.out.print("How many rows should be in the table: ");
+      row = ip.nextInt();
+    } while (row > 20);
+    
+    System.out.println();
+    
+    // Print new lines to the length of the column number
+    for (int i = 1; i <= row; i++) {
+    
+      // Print an entire row of numbers counting to the length
+      // of the row number, starting at 1.
+      for (int j = 1; j <= column; j++) {
+      
+        // Multiply both counters to make the multiplication table
+        int product = i * j;
+        System.out.printf("%4d", product);
+      }
+      // print a new line for the table.
+      System.out.printf("%n");
+    }  
+  }
+} // end class
---
+++ WordleWizard.java
@@ -0,0 +1,44 @@
+// Name: Kevin Koepp
+// CSE 174
+// 2-25-2022
+// This program scans other files inputted by the user and sends back
+// all 5-letter words (valid for the game Wordle) found within that
+// file.
+
+// Import three required scanners:
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+
+public class WordleWizard {
+  public static void main(String[] args) throws FileNotFoundException {
+  
+    Scanner kb = new Scanner(System.in);
+    
+    // Input the file to read:
+    System.out.print("Please enter the name of your Wordle file: ");
+    String fileName = kb.next();
+    
+    Scanner file = new Scanner(new File(fileName));
+    
+    System.out.println("");
+    
+    // Scan the file one word each for 5-Letter words:
+    int validWordle = 0;
+    while (file.hasNext()) {
+      String input = file.next();
+      
+      if (input.length() == 5) {
+        System.out.println(input);
+        validWordle++;
+      }      
+    }
+    
+    // Print out all words found and total number of words found.
+    System.out.printf("%n%s contained %d valid Wordle words.%n", 
+        fileName, validWordle);
+        
+  }
+} // end class
+  
+  
