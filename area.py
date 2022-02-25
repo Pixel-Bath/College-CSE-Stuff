264987d2327e5181283bb148c97ab39ab742a770
Kevin <98636238+Pixel-Bath@users.noreply.github.com>
2022-02-25 14:28:52 -0500

    Add files via upload

---
+++ area.py
@@ -0,0 +1,13 @@
+# Author: Kevin Koepp
+# Date: 2/18/2022
+'''
+Purpose: to find the area of a rectangle, given the
+length and width of the rectangle.
+'''
+
+length = int(input("Enter the length: "))
+width = int(input("Enter the width: "))
+
+area = length * width
+
+print("The area of a rectangle with length",length,"and width",width,"is",area) 
